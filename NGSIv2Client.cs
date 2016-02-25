ade26838e4eed77ec95e960d43299db33d06148e
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2016-02-25 10:02:05 +0100

    Removed unused usings

--- NGSIv2Client.cs
+++ NGSIv2Client.cs
@@ -17,10 +17,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Script.Serialization;
 
 namespace NGSIv2Plugin
 {
