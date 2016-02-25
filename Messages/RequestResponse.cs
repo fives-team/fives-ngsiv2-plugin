445040f91f0d0fc37428de290a7260396ba1bd47
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2016-02-25 13:08:13 +0100

    Added RequestResponse object to store results of Rest Requests

---
+++ Messages/RequestResponse.cs
@@ -0,0 +1,35 @@
+﻿// This file is part of FiVES.
+//
+// FiVES is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation (LGPL v3)
+//
+// FiVES is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with FiVES.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGSIv2Plugin.Messages
+{
+    public class RequestResponse
+    {
+        public object ResponseData { get; private set; }
+        public HttpStatusCode StatusCode { get; private set; }
+
+        public RequestResponse() { }
+        public RequestResponse(HttpStatusCode code, object data)
+        {
+            ResponseData = data;
+            StatusCode = code;
+        }
+    }
+}
