9dd51ed029aaaa4011461f59a71e220fad9de61f
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2016-09-08 09:28:02 +0200

    No longer registering a sinfoni interface that sneaked in during testign

--- NGSIv2PluginInitializer.cs
+++ NGSIv2PluginInitializer.cs
@@ -33,7 +33,6 @@ namespace NGSIv2Plugin
         public void Initialize()
         {
             RegisterNGSIComponent();
-            RegisterSinfoniService();
         }
 
         public string Name
@@ -58,20 +57,6 @@ namespace NGSIv2Plugin
             ngsi.AddAttribute<string>("id");
             ngsi.AddAttribute<string>("type");
             ComponentRegistry.Instance.Register(ngsi);
-        }
-
-        private void RegisterSinfoniService()
-        {
-            string idlContents = File.ReadAllText("ngsi.sinfoni");
-            SINFONIPlugin.SINFONIServerManager.Instance.SinfoniServer.AmendIDL(idlContents);
-            ClientManager.Instance.RegisterClientService("ngsi", true, new Dictionary<string, Delegate>{
-                {"createEntity", (Action<Dictionary<string, object>>)NGSIv2Processor.Instance.CreateEntity},
-                {"deleteEntity", (Action<string, bool>)NGSIv2Processor.Instance.DeleteEntity},
-                {"updateOrAppend",
-                    (Action<string,string,Dictionary<string,object>>)NGSIv2Processor.Instance.UpdateOrAppend},
-                {"retrieveEntityData", (Action<string>)NGSIv2Processor.Instance.RetrieveEntityData},
-                {"listEntities", (Action)NGSIv2Processor.Instance.RetrieveEntityCollection}
-            });
         }
     }
 }
